59f2e1ace0b092b448445a6089c00cb6c1dbb42f
kat <katwang7749@gmail.com>
2026-03-17 20:10:46 -0400

    add backgrounds and crash out

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- background
+<div class="relative w-screen">
+    <div class="absolute w-screen overflow-x-hidden -z-10">
+        <img
+            src="/landing/backgrounds/bg_full.png"
+            alt="background of landing page"
+        />
+    </div>
+</div> -->
+
+<!-- content -->
+<div class="relative w-screen bg-[url('landing/backgrounds/bg_full.png')] bg-cover">
+    <div class="bg-pink-200/50 h-[50vh] md:h-screen">
+        <div class="flex flex-row gap-[2vh]"></div>
+        <h1 class="text-white">hi</h1>
+    </div>
+
+    <div class=" h-screen p-[4vh] lg:p-[12vh]">
+        <div class="place-items-center">
+            <h1 class="text-blue-950 text-5xl font-bold">title.</h1>
+        </div>
+        <div class="flex flex-col lg:flex-row gap-[4vh]">
+            <h1 class="text-blue-950 text-2xl lg:text-5xl m-[1vh] lg:m-[4vh]">
+                yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap
+                yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap
+                yap yap
+            </h1>
+            <img
+                src="/landing/icon_webdevplanet.png"
+                alt="planet with text webdevplanet on top"
+                class="p-2 lg:p-30 w-[100vh]"
+            />
+        </div>
+    </div>
+
+    <div class="h-screen p-[12vh]">
+        <div class="place-items-center">
+            <h1 class="text-blue-950 text-5xl font-bold">title.</h1>
+        </div>
+        <div class="flex flex-col lg:flex-row-reverse gap-[4vh]">
+            <h1 class="text-blue-950 text-5xl m-[8vh]">
+                yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap
+                yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap
+                yap yap
+            </h1>
+            <img
+                src="/landing/icon_gamedevplanet.png"
+                alt="planet with text gamedevplanet on top"
+                class="w-[60vh] m-[8vh] p-[5vh]"
+            />
+        </div>
+    </div>    
+</div>
